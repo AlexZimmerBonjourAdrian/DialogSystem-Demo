35c343c70b6bd67c21e770da2e870e0d4a64269d
Alex <alexzimer.zimmer1@gmail.com>
2021-01-04 02:19:21 -0300

    Dialogue Editor Base

---
+++ Assets/Script/DialogueEditor/Editor/CDialogueGraph.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+public class CDialogueGraph : EditorWindow
+{
+    // Start is called before the first frame update
+ 
+        [MenuItem("Graph/Dialogue Graph")]
+     public static void OpenDialogueGraphwindow()
+    {
+
+        var window = GetWindow<CDialogueGraph>();
+        window.titleContent = new GUIContent(text: "Dialogue Graph");
+
+    }
+}
---
+++ Assets/Script/DialogueEditor/Editor/CDialogueGraphView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+
+public class CDialogueGraphView : GraphView
+{
+    // Start is called before the first frame update
+   public CDialogueGraphView()
+    {
+        this.AddManipulator(new ContentDragger());
+        this.AddManipulator(new SelectionDragger());
+        this.AddManipulator(new RectangleSelector());
+    }
+}
