d9d8da5336b1c0f1b1c86e03bd7c9bdf3d6e2014
Alex <alexzimer.zimmer1@gmail.com>
2021-01-04 13:27:16 -0300

    Draw Selection Box

--- Assets/Script/DialogueEditor/Editor/CDialogueGraph.cs
+++ Assets/Script/DialogueEditor/Editor/CDialogueGraph.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+
 public class CDialogueGraph : EditorWindow
 {
     // Start is called before the first frame update
- 
+
+    private CDialogueGraphView _graphView;
+
         [MenuItem("Graph/Dialogue Graph")]
      public static void OpenDialogueGraphwindow()
     {
@@ -14,5 +18,23 @@ public class CDialogueGraph : EditorWindow
         var window = GetWindow<CDialogueGraph>();
         window.titleContent = new GUIContent(text: "Dialogue Graph");
 
+    }
+
+    private void OnEnable()
+    {
+
+        _graphView = new CDialogueGraphView
+        {
+            name = "Dialogue Graph"
+        };
+
+        _graphView.StretchToParentSize();
+        rootVisualElement.Add(_graphView);
+
+    }
+
+    private void OnDisable()
+    {
+        rootVisualElement.Remove(_graphView);
     }
 }
