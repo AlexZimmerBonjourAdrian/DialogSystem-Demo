c85f4ed928f5cd26c1e48947fb5b8369df5a3841
Alex <alexzimer.zimmer1@gmail.com>
2021-01-08 17:02:09 -0300

    Excecute Create Node Window

--- Assets/Script/DialogueEditor/Editor/Runtime/CNodeSearchWindows.cs
+++ Assets/Script/DialogueEditor/Editor/Runtime/CNodeSearchWindows.cs
@@ -15,7 +15,12 @@ public class CNodeSearchWindows : ScriptableObject, ISearchWindowProvider
         var tree = new List<SearchTreeEntry>
         {
             new SearchTreeGroupEntry(new GUIContent (text: "Create Elements"), level:0),
-            new SearchTreeGroupEntry(new GUIContent(text: "Dialogue Node"),level:1)
+            new SearchTreeGroupEntry(new GUIContent(text: "Dialogue Node"),level:1),
+            new SearchTreeEntry(new GUIContent(text:"Dialogue Node"))
+            {
+                userData = new CDialogueNode(),level= 2
+            },
+           // new SearchTreeEntry (new GUIContent(text: "Hello world"))
         };
 
         return tree;
@@ -23,7 +28,14 @@ public class CNodeSearchWindows : ScriptableObject, ISearchWindowProvider
 
     public bool OnSelectEntry(SearchTreeEntry SearchTreeEntry, SearchWindowContext context)
     {
-        return true;
+        switch(SearchTreeEntry.userData)
+        {
+            case CDialogueNode dialogueNode:
+                Debug.Log(message: "Dialogue Node Created!!!");
+                return true;
+            default:
+                return false;
+        }
     }
    
 }
